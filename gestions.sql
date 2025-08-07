8254012939a65a68188ae0ad74a2fac4b535af4f
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2025-08-07 23:15:31 +0100

    update

--- gestions.sql
+++ gestions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 05 août 2025 à 19:23
+-- Généré le : jeu. 07 août 2025 à 23:43
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `message` text NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `chat`
+--
+
+INSERT INTO `chat` (`id`, `user_id`, `nom`, `prenom`, `photo`, `message`, `created_at`) VALUES
+(23, 15, 'MEWO', 'INES', '1754600666_Beidi.png', 'bonjour ici', '2025-08-07 17:34:43'),
+(24, 13, 'BEIDI', 'SAMUEL', '1754436857_sammmmm.png', 'bonjour', '2025-08-07 17:35:09'),
+(25, 13, 'BEIDI', 'SAMUEL', '1754436857_sammmmm.png', 'hi', '2025-08-07 17:38:00'),
+(26, 13, 'BEIDI', 'SAMUEL', '1754436857_sammmmm.png', 'bhn', '2025-08-07 17:38:33');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `comments`
 --
 
@@ -34,6 +60,17 @@ CREATE TABLE `comments` (
   `content` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `content`, `created_at`) VALUES
+(1, 1, 13, 'hi', '2025-08-05 17:36:46'),
+(2, 4, 13, 'un test de commentaire', '2025-08-05 23:33:49'),
+(3, 5, 14, 'salut', '2025-08-06 01:51:17'),
+(4, 5, 13, 'j', '2025-08-07 16:05:56'),
+(5, 0, 13, 'hi', '2025-08-07 20:00:57');
 
 -- --------------------------------------------------------
 
@@ -81,7 +118,34 @@ CREATE TABLE `etudiants` (
 --
 
 INSERT INTO `etudiants` (`id`, `nom`, `prenom`, `matricule`, `numero`, `filiere`, `bio`, `email`, `mot_de_passe`, `photo_profil`) VALUES
-(13, 'BEIDI', 'SAMUEL', '18A1093FS', '697241071', 'IRS', 'ingeriere', 'beidisamuel11@gmail.com', '$2y$10$ydoOh301Ba7qkoPGQW/yYe8Q1d5IqCNRYkl5onPZez7DzQvQJYBtK', '1754411303_logo.jpg');
+(13, 'BEIDI', 'SAMUEL', '18A1093FS', '697241071', 'IRS', 'Informaticien', 'beidisamuel11@gmail.com', '$2y$10$ydoOh301Ba7qkoPGQW/yYe8Q1d5IqCNRYkl5onPZez7DzQvQJYBtK', '1754436857_sammmmm.png'),
+(14, 'ABdoulatif', 'as', '101', '698', 'IHN', NULL, 'abdou@gmail.com', '$2y$10$/e2uE9hceYb/juNjEXHNhOh1hykTPgXocTADzVIVi/OaYLeDG0C9a', '1754438719_dddd.JPG'),
+(15, 'MEWO', 'INES', '1akdm', '697', 'INFO', NULL, 'mewo@gmail.com', '$2y$10$bodFjo24g3jdB5cul6KLye0LwWCYq7MbA5DpeJ1jgmGRCit0L9eAy', '1754600666_Beidi.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `likes`
+--
+
+INSERT INTO `likes` (`id`, `user_id`, `post_id`, `created_at`) VALUES
+(81, 14, 5, '2025-08-06 01:57:58'),
+(87, 14, 3, '2025-08-06 01:58:08'),
+(90, 14, 1, '2025-08-06 01:58:19'),
+(237, 13, 1, '2025-08-07 16:06:26'),
+(240, 13, 6, '2025-08-07 21:38:12'),
+(241, 13, 5, '2025-08-07 21:38:14');
 
 -- --------------------------------------------------------
 
@@ -99,6 +163,20 @@ CREATE TABLE `news` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `posts`
 --
 
@@ -109,6 +187,18 @@ CREATE TABLE `posts` (
   `file_path` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `content`, `file_path`, `created_at`) VALUES
+(1, 13, 'sc', '68923f6b6e509.jpg', '2025-08-05 17:29:15'),
+(2, 13, 'resultats SN', '6892415ab061c.pdf', '2025-08-05 17:37:30'),
+(3, 13, 'Jeune', NULL, '2025-08-05 18:03:54'),
+(4, 13, 'Juste envie de poster', '689294c539968.jpeg', '2025-08-05 23:33:25'),
+(5, 14, 'ok', NULL, '2025-08-06 01:35:43'),
+(6, 14, 'Hi', NULL, '2025-08-06 01:51:30');
 
 -- --------------------------------------------------------
 
@@ -133,6 +223,12 @@ CREATE TABLE `users` (
 --
 
 --
+-- Index pour la table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `comments`
 --
 ALTER TABLE `comments`
@@ -153,9 +249,22 @@ ALTER TABLE `etudiants`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_like` (`user_id`,`post_id`);
+
+--
 -- Index pour la table `news`
 --
 ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `notifications`
+--
+ALTER TABLE `notifications`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -177,10 +286,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `department_news`
@@ -192,7 +307,13 @@ ALTER TABLE `department_news`
 -- AUTO_INCREMENT pour la table `etudiants`
 --
 ALTER TABLE `etudiants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT pour la table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=243;
 
 --
 -- AUTO_INCREMENT pour la table `news`
@@ -201,10 +322,16 @@ ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
